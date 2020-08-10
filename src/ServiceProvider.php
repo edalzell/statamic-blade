b51addf5304ffefd102646757edbb036a24342ee
edalzell <erin@thedalzells.org>
2020-08-09 18:25:50 -0700

    refactor

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -22,23 +22,31 @@ class ServiceProvider extends AddonServiceProvider
 
     private function bootBard()
     {
-        Blade::directive('bard', function ($expression) {
-            return "<?php foreach (Facades\Edalzell\Blade\Directives\Bard::handle(${expression}) as \$set) { ?>";
-        });
+        Blade::directive(
+            'bard',
+            fn ($expression) => $this->startPHP("foreach (Facades\Edalzell\Blade\Directives\Bard::handle(${expression}) as \$sets)")
+        );
 
-        Blade::directive('endbard', function () {
-            return '<?php } ?>';
-        });
+        Blade::directive('endbard', fn () => $this->endPHP());
     }
 
     private function bootCollection()
     {
-        Blade::directive('collection', function ($expression) {
-            return "<?php foreach (Facades\Edalzell\Blade\Directives\Collection::handle(${expression}) as \$entry) { ?>";
-        });
+        Blade::directive(
+            'collection',
+            fn ($expression) => $this->startPHP("foreach (Facades\Edalzell\Blade\Directives\Collection::handle(${expression}) as \$entry)")
+        );
 
-        Blade::directive('endcollection', function () {
-            return '<?php } ?>';
-        });
+        Blade::directive('endcollection', fn () => $this->endPHP());
+    }
+
+    private function startPHP($php)
+    {
+        return "<?php {$php} { ?>";
+    }
+
+    private function endPHP()
+    {
+        return '<?php } ?>';
     }
 }
