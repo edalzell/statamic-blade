fcfc2b635f1e85e1f8fa3694dac43272a829b769
edalzell <erin@thedalzells.org>
2020-08-09 18:26:01 -0700

    simple bard test

--- tests/Unit/DirectivesTest.php
+++ tests/Unit/DirectivesTest.php
@@ -7,10 +7,19 @@ use Edalzell\Blade\Tests\TestCase;
 class DirectivesTest extends TestCase
 {
     /** @test */
-    public function does_display_correctly()
+    public function does_display_collection_correctly()
     {
         $blade = "@collection('foo')";
-        $expected = "<?php foreach (Edalzell\Blade\Facades\Blade::collection('foo') as \$entry) { ?>";
+        $expected = "<?php foreach (Facades\Edalzell\Blade\Directives\Collection::handle('foo') as \$entry) { ?>";
+
+        $this->assertSame($expected, $this->blade->compileString($blade));
+    }
+
+    /** @test */
+    public function does_display_bard_correctly()
+    {
+        $blade = "@bard([])";
+        $expected = "<?php foreach (Facades\Edalzell\Blade\Directives\Bard::handle([]) as \$sets) { ?>";
 
         $this->assertSame($expected, $this->blade->compileString($blade));
     }
