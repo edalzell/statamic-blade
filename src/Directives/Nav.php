337f351fd403743184b99f0a748a3aa44c7623f9
edalzell <erin@thedalzells.org>
2020-10-01 22:10:10 -0700

    set default params (#4)

--- src/Directives/Nav.php
+++ src/Directives/Nav.php
@@ -7,16 +7,24 @@ use Statamic\Tags\Structure;
 
 class Nav extends Structure
 {
-    public function __construct()
+    public function handleNav(string $handle, $params = [])
+    {
+        $this->initParams($params);
+
+        return $this->structure($handle);
+    }
+
+    private function initParams($params = [])
     {
         // Because this extends the `structure` tag, have to fake the parameters
         // Can't use Parameter because that expects even more variables to be set
         // @TODO is there a nicer way to do this?
-        $this->params = ArrayAccessor::make(['include_home' => false]);
-    }
+        $defaults = [
+            'from' => null,
+            'show_unpublished' => false,
+            'include_home' => false,
+        ];
 
-    public function handleNav(string $handle)
-    {
-        return $this->structure($handle);
+        $this->params = ArrayAccessor::make(array_merge($defaults, $params));
     }
 }
