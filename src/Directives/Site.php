2f270bbe1f88133382a3917d61ebdad9e38ac91a
edalzell <erin@thedalzells.org>
2021-01-31 16:21:01 -0800

    site is diff than others (#23)

--- src/Directives/Site.php
+++ src/Directives/Site.php
@@ -3,6 +3,7 @@
 namespace Edalzell\Blade\Directives;
 
 use Edalzell\Blade\Concerns\IsDirective;
+use Illuminate\Support\Facades\Blade;
 use Statamic\Sites\Sites;
 use Statamic\Support\Arr;
 
@@ -23,6 +24,25 @@ class Site
     public function handleKey(string $key = null)
     {
         return Arr::get($this->getAugmentedValue($this->site()), $key);
+    }
+
+    public function bootBoth()
+    {
+        Blade::directive(
+            $this->directive,
+            function ($expression) {
+                if ($expression) {
+                    return $this->asString(get_class($this), "{$this->method}Key", $expression);
+                }
+
+                return $this->asArray($this->key, get_class($this), $this->method, $expression);
+            }
+        );
+
+        Blade::directive(
+            "end{$this->directive}",
+            fn () => $this->endAsArray($this->key)
+        );
     }
 
     private function site()
