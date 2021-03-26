c4085e5b030c95b672ebddd2f68dd2ae34b1fd91
Adam Campbell <adam@hotmeteor.com>
2021-03-26 09:08:51 -0500

    Update Breadcrumbs.php

--- src/Directives/Breadcrumbs.php
+++ src/Directives/Breadcrumbs.php
@@ -4,23 +4,67 @@ namespace Edalzell\Blade\Directives;
 
 use Edalzell\Blade\Concerns\IsDirective;
 use Statamic\Facades\Antlers;
-use Statamic\Tags\Structure;
+use Statamic\Facades\Data;
+use Statamic\Facades\Site;
+use Statamic\Facades\URL;
+use Statamic\Support\Str;
+use Statamic\Tags\Nav as NavTag;
 
-class Breadcrumbs extends Structure
+class Breadcrumbs extends NavTag
 {
     use IsDirective;
 
     protected string $directive = 'breadcrumbs';
-    protected string $key = 'variables';
-    public $method = 'handleBreadcrumbs';
+    protected string $key = 'item';
     protected string $type = 'loop';
+    public $method = 'handleBreadcrumbs';
 
-    public function handleBreadcrumbs($context, $params = [])
+    public function handleBreadcrumbs(array $params = [])
     {
         $this->setParser(Antlers::parser());
-        $this->setContext($context);
+        $this->setContext([]);
         $this->setParameters($params);
 
         return $this->breadcrumbs();
+    }
+
+    public function breadcrumbs()
+    {
+        $currentUrl = URL::makeAbsolute(URL::getCurrent());
+        $url = Str::removeLeft($currentUrl, Site::current()->absoluteUrl());
+        $url = Str::ensureLeft($url, '/');
+        $segments = explode('/', $url);
+        $segments[0] = '/';
+
+        if (! $this->params->bool('include_home', true)) {
+            array_shift($segments);
+        }
+
+        $crumbs = collect($segments)->map(function () use (&$segments) {
+            $uri = URL::tidy(join('/', $segments));
+            array_pop($segments);
+
+            return $uri;
+        })->mapWithKeys(function ($uri) {
+            $uri = Str::ensureLeft($uri, '/');
+
+            return [$uri => Data::findByUri($uri, Site::current()->handle())];
+        })->filter();
+
+        if (! $this->params->bool('reverse', false)) {
+            $crumbs = $crumbs->reverse();
+        }
+
+        if ($this->params->bool('trim', true)) {
+            $this->content = trim($this->content);
+        }
+
+        $crumbs = $crumbs->values()->map(function ($crumb) {
+            $crumb->setSupplement('is_current', URL::getCurrent() === $crumb->url());
+
+            return $crumb;
+        });
+
+        return $crumbs->toAugmentedArray();
     }
 }
